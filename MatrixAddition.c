13092f8b6801837cdea8d931163a69df8a001aa1
Akshat1609 <65245150+Akshat1609@users.noreply.github.com>
2020-09-30 19:46:44 +0530

    Update MatrixAddition.c

--- MatrixAddition.c
+++ MatrixAddition.c
@@ -1,7 +1,9 @@
+
 #include<stdio.h>
+//Rather than creating other matrix c, directly print the sum of two matrix in matrix form
 int main()
 {
-   int a[100][100],b[100][100],c[100][100],i,j,m,n;
+   int a[100][100],b[100][100],i,j,m,n;
    printf("Enter the size of the matrix \n");
    scanf("%d%d", &m, &n);
    printf("Enter the First matrix\n");
@@ -20,19 +22,13 @@ int main()
 	   scanf("%d",&b[i][j]);
 	}
    }
-   for(i=0;i<m;i++)
-   {
-	for(j=0;j<n;j++)
-	{
-	   c[i][j]=a[i][j]+b[i][j];
-	}
-   }
+
    printf("\nThe Addition of two matrix is\n");
    for(i=0;i<m;i++)
    {
 	for(j=0;j<n;j++)
 	{
-	   printf("%d\t",c[i][j]);
+	   printf("%d\t",a[i][j]+b[i][j]);
 	}
    printf("\n");
    }
